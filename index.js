824b737efe537732d6e72adc9046681b1074308e
olenazubashenko <olenazubashenko@gmail.com>
2019-09-23 16:04:46 +0300

    new changes

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+ 
+
+
+
+let youtubeBtn = document.getElementById('youtubeClose');
+let video = document.getElementsByClassName('video__btn')[0];
+let show = document.getElementById('youtube');
+
+
+let rates = document.getElementById('rates');
+let closeRates = document.getElementById('rates-close');
+let additionally = document.getElementsByClassName ('additionally')[0];
+
+video.onclick = function() {
+    youtubeBtn.style.display = 'block',
+    show.style.display = 'block';
+}
+
+youtubeBtn.onclick = function() {
+    youtubeBtn.style.display = 'none',
+    show.style.display = 'none';
+}
+
+
+additionally.onclick = function () {
+    rates.style.display = 'block';
+}
+
+closeRates.onclick = function() {
+    rates.style.display = 'none';
+}
+
+
+
+
 let showYouTube = ()=>{
-    $('#youtube').removeAttr('hidden');
+    $('#youtube').removeAttr('hidden')
+   
 };
+
+let youtubeClose = () => {
+    $('#youtube').attr('hidden', true);
+}
